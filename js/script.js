8237081825fd372bb99fd581e2795d0b99d5b706
eilinseth <aksun989@gmail.com>
2024-05-29 19:38:06 +0700

    Update style mencegah anchor mengarah ke atas

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ const navbar = document.querySelector('.navbar-nav');
 const hamburger = document.getElementById('hamburger-menu');
 hamburger.addEventListener('click',function(){
     navbar.classList.toggle('active');
+    e.preventDefault();
 })
 
 document.addEventListener('click',function(e){
+    
     if(!hamburger.contains(e.target) && !navbar.contains(e.target)){
         navbar.classList.remove('active');
     }
+    // e.preventDefault();
 })
