1660cdd1f51558646a8a687ab847649469fceaae
eilinseth <aksun989@gmail.com>
2024-05-31 14:56:58 +0700

    membuat tampilan untuk search

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 const navbar = document.querySelector('.navbar-nav');
 const hamburger = document.getElementById('hamburger-menu');
-hamburger.addEventListener('click',function(){
+hamburger.addEventListener('click',function(e){
     navbar.classList.toggle('active');
     e.preventDefault();
 })
 
-document.addEventListener('click',function(e){
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+const searchBtn = document.querySelector('#search-btn');
+
+searchBtn.addEventListener('click',function(e){
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+})
+
+document.addEventListener('click',function(event){
     
-    if(!hamburger.contains(e.target) && !navbar.contains(e.target)){
+    if(!hamburger.contains(event.target) && !navbar.contains(event.target)){
         navbar.classList.remove('active');
-    }
-    // e.preventDefault();
+    };
+    if(!searchForm.contains(event.target) && !searchBtn.contains(event.target)){
+        searchForm.classList.remove('active');
+    };
+    
+    
 })
